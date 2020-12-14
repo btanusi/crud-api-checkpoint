eb02090655a104eede7a1ca9f554c0c5a3ef6382
Brianna Tanusi <btanusi@gmail.com>
2020-12-14 09:55:00 -0800

    cleaned up code

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -2,11 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Date;
-
 public interface UserRepository extends CrudRepository<User, Long> {
-    /*
-    User findByTitle(String title);
-    Iterable<User> findByDeliveredOnBetween(Date date1, Date date2);
-     */
+    User findByEmail(String email);
 }
